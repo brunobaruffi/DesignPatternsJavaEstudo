186598c06b48a242ef163932523c91418b1e3e1f
brunobaruffi <brunobaruffi@gmail.com>
2023-11-15 20:59:12 -0300

    ajust

--- Interceptor/APIExemplo/src/main/java/org/example/repository/alunos.java
+++
@@ -1,8 +0,0 @@
-package org.example.repository;
-
-import org.example.entity.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface alunos extends JpaRepository<Aluno, Integer> {
-
-}
--- Interceptor/README.md
+++
@@ -1,4 +0,0 @@
-# Modelo base de API
-
-É um modelo base de API para ser reutilizado em estudos de outros recursos.
-Possui uma conexao com banco de dados e sua estrura basica de API
